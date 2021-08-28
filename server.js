8f6ccb1c618b462876cb9b42bf80a1f89f92c247
shubhambattoo <shubhambattoo@hotmail.com>
2021-08-28 13:12:06 +0530

    updated packages

--- server.js
+++ server.js
@@ -1,30 +1,25 @@
 /* eslint-disable no-console */
-require("dotenv").config();
-const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-process.on("uncaughtException", err => {
-  console.log("Unhandler Exception! Shutting Down...");
+process.on('uncaughtException', err => {
+  console.log('Unhandler Exception! Shutting Down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
 mongoose
-  .connect(process.env.DATABASE_HOST+process.env.DATABASE_NAME, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  })
+  .connect(process.env.DATABASE_HOST + process.env.DATABASE_NAME)
   .then(() => {
     console.log(`${process.env.DATABASE_NAME} db connected successfully`);
   })
   .catch(err => {
     console.log(err);
-    console.log("db error");
+    console.log('db error');
     process.exit(1);
   });
 
-const app = require("./app");
+const app = require('./app');
 
 const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => {
